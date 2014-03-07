b840a21689d1b05cd743acfbc64651ee5ca546ce
Lenny Dadu <ledadu@gmail.com>
2014-03-07 21:56:29 +0100

    refactorring

---
+++ Players.js
@@ -0,0 +1,45 @@
+var _ = require('underscore');
+//var world = require('./World');
+var Players = function() {
+    this.list={};
+    //construct
+}
+
+Players.prototype.removeplayer = function (pp) {
+        newplayers = {};
+        _.each(this.list, function(p) {
+            if (p.id != pp.id) {
+                newplayers[p.id] = pp;
+            }
+        });
+        this = newplayers;
+    }
+    
+Players.prototype.spawnAll = function () {    
+    _.each(this.list, function(p) {
+        p.spawn();
+    });
+}
+
+Players.prototype.countPlayerNotDead = function () { 
+    countplayernotdead = 0;
+    _.each(this.list, function(p) {	//todo _.find
+        if (p.direction != "dead") {
+            lastplayer = p;
+            countplayernotdead++;
+        }
+    });
+    if(countplayernotdead>1){
+        return countplayernotdead;
+    }else
+    if(countplayernotdead==1){
+        return 1
+    }else
+    if(countplayernotdead==0){
+        return 0;
+    }
+}
+
+
+
+module.exports = Players;
