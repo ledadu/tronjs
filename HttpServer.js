3ed38b984a37c2baade6ca8233907297bbdf85fa
Lenny Dadu <ledadu@gmail.com>
2014-03-13 23:01:05 +0100

    static express

--- HttpServer.js
+++ HttpServer.js
@@ -1,29 +1,37 @@
 //var fs = require('fs');
 
 
+var Express = require('express');
 var HttpServer = function(tcport) {
     //construct
-    //this.server = require('http').createServer(handler);
-    this.app = require('express')();
+    //this.server = require('http').createServer(handler);    
+    this.app = new Express();
     this.server = require('http').createServer(this.app);    
     this.server.listen(tcport);
 };
 
 
 HttpServer.prototype.configure = function() {
+    var that = this;
+//    this.app.use(function(req, res, next){
+//        if (req.path == '/') {
+//            req.path = '/index.html';
+//        }
+//        console.log(req.query);
+//        res.sendfile(__dirname + '/public' + req.path);
+//      });
+      
 
-    this.app.use(function(req, res, next){
-        if (req.url == '/') {
-            req.url = '/index.html';
-        }
-        console.log(req.url);
-        res.sendfile(__dirname + '/public' + req.url);
-      });
-//  that.server.set('views', __dirname + '/views');
+      
+//      this.app.get('/user/:id?', function(req, res){
+//          console.log(req.route);
+//        res.sendfile(__dirname + '/public/index.html');
+//      });
+//  this.app.set('views', __dirname + '/views');
 //  that.server.set('view engine', 'jade');
-//  that.server.use(Express.favicon());
+//  this.app.use(Express.favicon());
 //  that.server.use(Express.logger('dev'));
-//  that.server.use(Express.static(__dirname + '/public'));
+    this.app.use(Express.static(__dirname + '/public'));
 //  that.server.use(Express.bodyParser());
 //  that.server.use(Express.methodOverride());
 //  that.server.use(that.server.router);
